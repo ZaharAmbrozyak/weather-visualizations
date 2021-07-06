972f0ed31cbe2df456b1b007691ea4ef5f49f9b5
zahar <zaharambrozyak@gmail.com>
2021-07-06 15:34:14 +0300

    Death Valley prcp@

--- death_valley_prcp.py
+++ death_valley_prcp.py
@@ -1,15 +1,16 @@
 from csv import reader
+from os import read
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'data/sitka_weather_2018_simple.csv'
+filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
      reader = reader(f)
-
+     
      header_row = next(reader)
 
-     #0STATION 1NAME 2DATE 3PRCP 4TAVG 5TMAX 6TMIN
-     prcp, dates = [], []
+     #0STATION 1NAME 2DATE 3PRCP 4TMAX 5TMIN 6TOBS
+     dates, prcp = [], []
 
      for row in reader:
           current_date = datetime.strptime(row[2], '%Y-%m-%d')
@@ -22,12 +23,12 @@ plt.style.use('seaborn')
 fig, ax = plt.subplots()
 plt.plot(dates, prcp, c='blue')
 
-title = 'Daily prcp - 2018\n Sitka'
+title = 'Daily prcp - 2018\n Death Valley, CA'
 plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=14)
 plt.ylabel('prcp', fontsize=14)
 plt.tick_params(axis='both', which='major', labelsize=14)
 fig.autofmt_xdate()
 
-# plt.savefig(title, bbox_inches='tight')
+# plt.savefig(title,bbox_inches='tight')
 plt.show()
